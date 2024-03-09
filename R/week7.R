890a454f34e6647b84dd5f42e326a9bbf48c70cb
Jessica Wang <wan00967@umn.edu>
2024-03-08 19:32:05 -0600

    changed condition type

--- R/week7.R
+++ R/week7.R
@@ -1,7 +1,7 @@
 # Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
-
+library(GGally)
 
 
 
@@ -11,6 +11,7 @@ week7_tbl <- read_csv("../data/week3.csv") %>%
          timeEnd = as.POSIXct(timeEnd, format = "%Y-%m-%d %H:%M:%S"),
          timeSpent = as.numeric(difftime(timeEnd, timeStart, units = "mins"))) %>%  #why is it negative?? i can't fix it
   mutate(across(q1:q10, as.numeric)) %>%
+  mutate(condition = factor(condition, levels = c("A", "B", "C"), labels = c("Block A", "Block B", "Control"))) %>%
   mutate(gender = factor(gender, levels = c("M", "F"), labels = c("Male", "Female"))) %>%
   filter(q6 == 1) %>%
   select(-q6)
