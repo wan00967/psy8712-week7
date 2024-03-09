0616ed3eb2f4ee4cc7405fc71605dbf2627efbd8
Jessica Wang <wan00967@umn.edu>
2024-03-09 12:11:49 -0600

    project complete

--- R/week7.R
+++ R/week7.R
@@ -15,3 +15,42 @@ week7_tbl <- read_csv("../data/week3.csv") %>%
   mutate(gender = factor(gender, levels = c("M", "F"), labels = c("Male", "Female"))) %>%
   filter(q6 == 1) %>%
   select(-q6)
+
+
+
+# Visualization
+ggpairs(week7_tbl %>% 
+          select(q1, q2, q3, q4, q5, q7, q8, q9, q10))
+
+(week7_tbl %>%
+  ggplot(aes(x = timeStart, y = q1)) +
+  geom_point() +
+  labs(x = "Time Start", y = "Q1 Score")) %>%
+  ggsave("../figs/fig1.png", ., dpi = 600, height = 3, width = 5)
+  
+(week7_tbl %>% 
+  ggplot(aes(x = q1, y = q2, color = gender)) +
+  geom_jitter())%>%
+  ggsave("../figs/fig2.png", ., dpi = 600, height = 4, width = 6)
+
+(week7_tbl %>%
+  ggplot(aes(x = q1, y = q2)) +
+  geom_jitter() +
+  facet_wrap(~gender) +
+  labs(x = "Score on Q1", y = "Score on Q2")) %>%
+  ggsave("../figs/fig3.png", ., dpi = 600, height = 3, width = 6)
+
+(week7_tbl %>%
+    ggplot(aes(x = gender, y = timeSpent)) +
+  geom_boxplot() +
+  labs(x = "Gender", y = "Time Spent (mins)")) %>%
+  ggsave("../figs/fig4.png", ., dpi = 600, height = 4, width = 7.25)
+
+(week7_tbl %>%
+  ggplot(aes(x = q5, y = q7, color = condition)) +
+  geom_jitter(width = 0.2, height = 0.2) +  
+  geom_smooth(method = "lm", se = FALSE) +  
+  labs(x = "Score on Q5", y = "Score on Q7", color= "Experimental Condition") +
+  theme(legend.position = "bottom", legend.background = element_rect(fill = "#DFDFDF"))) %>%
+  ggsave("../figs/fig5.png", ., dpi = 600, height = 4, width = 7)
+
